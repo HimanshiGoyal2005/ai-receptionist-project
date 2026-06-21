d35aa2573fcc1a6fc63d6103a42e146514b0b9d0
HimanshiGoyal2005 <himanshi3573@gmail.com>
2026-06-21 21:18:01 +0530

    errors solved

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,8 @@
 import axios from 'axios'
 
-console.log(import.meta.env.VITE_API_URL)
-
 const API = axios.create({
+  // Vite mein variable access karne ka sahi tarika:
   baseURL: import.meta.env.VITE_API_URL
-})
-
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem('token')
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-
-  return config
 })
 
 export default API
